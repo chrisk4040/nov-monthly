37dfbb4654c354b3b1e3f9b30bf7bd1cdf0e7365
chrisk4040 <526ckozeniesky@frhsd.com>
2024-11-20 20:52:44 -0500

    just in case need to restart

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
-let userName = prompt('what is your name!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let userName = prompt('what is your name!')
 let userQuest = prompt('what is your quest?!')
 let userFavColor = prompt('What is your favorite color?')
 let swallowAirspeed = prompt('What is the air speed of an unlaiden swallow?')
 
 
-let myStory = ` <p>
+
+let myStory = `<p>
 Hello ${userName}. Answer me these questions three and the other side you'll see.</p>
 
 <p> I see you have already andertrf, do your quest ${userQuest}.
@@ -12,4 +41,4 @@ Hello ${userName}. Answer me these questions three and the other side you'll see
 `
 console.log(myStory)
 
-document.getElementById('story') .innerHTML = myStory
+document.getElementById('story') .innerHTML = myStory  */
