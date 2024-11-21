5ed46832586214c7ecbde9029cd491946d0e0439
chrisk4040 <526ckozeniesky@frhsd.com>
2024-11-21 09:31:18 -0500

    fixed java neeed to style

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('startquest').addEventListener('click', function() {
+/*document.getElementById('startquest').addEventListener('click', function() {
 
 let adjective = prompt("Enter an adjective:")
 let noun = prompt("Enter a noun:")
@@ -6,15 +6,19 @@ let verb = prompt("Enter a verb:")
 let place = prompt("Enter a place:")
 let hero = prompt("Enter hero's Name:")
 
-let Story = `<p>In the land of ${place}, there lived a ${adjective} hero named ${hero}.</p> 
+let Story = ` the land of ${place}, there lived a ${adjective} hero named ${hero}.
 
-<p>One day, ${hero} ${verb} a fearsome ${noun} that threatened the kingdom.</p> 
+One day, ${hero} ${verb} a fearsome ${noun} that threatened the kingdom.
 
-<p>The battle was fierce, but ${hero} 
-emerged victorious, bringing peace back to ${place}.</p>`
+The battle was fierce, but ${hero} 
+emerged victorious, bringing peace back to ${place}.`
 
-document.getElementById('story').innerText = story;
-});
+document.getElementById('story').innerText = Story;
+});*/
+
+var w = someValue;
+document.getElementById("mytext").value = w;
+echo $_POST['user'];
 
 
 
